d195be4d92dbc521e648b18e74bee20a84593506
Krishan Gupta <krishan57gupta@gmail.com>
2020-04-18 13:17:36 +0530

    Update test_divideBy.R

--- inst/unitTests/test_divideBy.R
+++ inst/unitTests/test_divideBy.R
@@ -1,5 +1,4 @@
 test_divideBy <- function() {
-    checkEquals(divideBy(8, 2), 2)
+    checkEquals(divideBy(8, 4), 2)
     checkTrue(is.na(divideBy(10, 0)))
-    checkEqualsNumeric(divideBy(5, 1.2345), 3.24, tolerance=1.0e-4)
 }
